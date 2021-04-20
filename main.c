0ba14274e1d07ef042c1cbaa9bfaacee6aa05a9f
noname <lewartamonov@gmail.com>
2021-04-20 14:34:00 +0200

    Move en/decrypt actions to functions

--- main.c
+++ main.c
@@ -8,13 +8,16 @@
 
 
 void usage(void);
+void encrypt(char *data, const char *key, int data_len, int key_len);
+void decrypt(char *data, const char *key, int data_len, int  key_len);
 
 
 time_t seconds;
 int fd;
-unsigned char key[8];
+unsigned char key[1024];
 char *message;
 int message_len;
+int key_length = 0;
 
 
 int main(int argc, char const *argv[]) {
@@ -32,21 +35,34 @@ int main(int argc, char const *argv[]) {
 	strcpy(message, argv[1]);
 	message_len = strlen(message);
 
-	for (short i = 0; i < 8; i++) {
-		read(fd, key, 8);
+	for (int i = 0; i < 1024; i++) {
+		char buffer;
+		if (read(fd, &buffer, 1) == 0) {
+			key_length = i + 1;
+			break;
+		}
+		else {
+			key[i] = buffer;
+		}
 	}
+	if (key_length == 0)
+		key_length = 1024;
 
-	printf("Key: ");
-	for (int i = 0; i < 8; i++) {
-		printf("%x ", key[i]);
+	printf("Key length: %d\n", key_length);
+
+	printf("Key:\n");
+	for (int i = 0; i < key_length; i += 16) {
+		for (int j = 0; j < 16; j++) {
+			if (j + i < key_length)
+				printf("%x ", key[i + j]);
+		}
+		printf("\n");
 	}
 	printf("\n");
 
 	printf("Message: %s\n", message);
 
-	for (int i = 0; i < message_len; i++) {
-		message[i] = message[i] ^ key[i % 8];
-	}
+	encrypt(message, key, message_len, key_length);
 
 	printf("Crypted message: ");
 	for (int i = 0; i < message_len; i++) {
@@ -54,9 +70,7 @@ int main(int argc, char const *argv[]) {
 	}
 	printf("\n");
 
-	for (int i = 0; i < message_len; i++) {
-		message[i] = message[i] ^ key[i % 8];
-	}
+	decrypt(message, key, message_len, key_length);
 
 	printf("Derypted message: ");
 	for (int i = 0; i < message_len; i++) {
@@ -67,4 +81,18 @@ int main(int argc, char const *argv[]) {
 	free(message);
 	close(fd);
 	return 0;
+}
+
+
+void encrypt(char *data, const char *key, int data_len, int key_len) {
+	for (int i = 0; i < data_len; i++) {
+		data[i] = data[i] ^ key[i % key_len];
+	}
+}
+
+
+void decrypt(char *data, const char *key, int data_len, int key_len) {
+	for (int i = 0; i < data_len; i++) {
+		data[i] = data[i] ^ key[i % key_len];
+	}
 }
